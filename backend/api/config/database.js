99ddfac14c94c8c26502448842ac0d43fc4b676a
ArthurCorreaEnterprise <arthur.correa@DSS.LOCAL>
2024-10-02 05:59:10 -0400

    Configure: PostGreSQL Database

--- backend/api/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// Crie uma instância do Sequelize com as credenciais do banco de dados
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-});
-
-module.exports = sequelize;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
-require('dotenv').config(); // Adicione esta linha no topo do arquivo
+// server.js
 
-const express = require('express');
-const db = require('./api/models');
+const express = require("express");
+const bodyParser = require("body-parser");
+const userRoutes = require("./api/routes/userRoutes");
+const accessRoutes = require("./api/routes/accessRoutes");
+const db = require("./api/models"); // Importar modelos
 
 const app = express();
+app.use(bodyParser.json());
+
+// Usar rotas
+app.use("/api", userRoutes);
+app.use("/api", accessRoutes);
+
 const PORT = process.env.PORT || 4000;
 
-// Teste a conexão com o banco de dados
-db.sequelize.authenticate()
-    .then(() => {
-        console.log('Conexão com o banco de dados estabelecida com sucesso.');
-    })
-    .catch(err => {
-        console.error('Não foi possível conectar ao banco de dados:', err);
+db.sequelize
+  .sync() // Sincronizar os modelos com o banco de dados
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+  })
+  .catch((error) => {
+    console.error("Erro ao sincronizar modelos:", error);
+  });
