b9562a186890c7a6e6ce9d99dfa40dc40c52ac3a
Arthur de Paula Correa <arthurdepaulacorrea@hotmail.com>
2024-10-07 11:05:30 -0300

    Develop: Style

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,41 @@
-// import './assets/main.css'
+import './assets/tailwind.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+import {
+  faArrowLeft,
+  faArrowsRotate,
+  faEnvelope,
+  faKey,
+  faMagnifyingGlass,
+  faMoon,
+  faRightFromBracket,
+  faRightToBracket,
+  faSun,
+  faUser,
+  faXmark
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faMagnifyingGlass,
+  faRightFromBracket,
+  faXmark,
+  faEnvelope,
+  faKey,
+  faArrowLeft,
+  faRightToBracket,
+  faGoogle,
+  faUser,
+  faMoon,
+  faSun,
+  faArrowsRotate
+)
 
 const app = createApp(App);
 
@@ -11,5 +43,6 @@ const app = createApp(App);
 store.dispatch('tryAutoLogin');
 
 app.use(store);
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router);
 app.mount('#app');
